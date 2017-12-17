d7f89e657b2b67b2b0f8734873a98cc7b2ee6881
Saim Othman <saimothmane71@gmail.com>
2017-12-17 14:30:19 +0000

    refactoring the server code and adding comments

--- server/app.js
+++ server/app.js
@@ -1,41 +1,55 @@
 var express = require('express');
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var cors = require("cors");
+
 var app = express();
 var port = 3000;
-var MongoClient = require('mongodb').MongoClient;
-var ObjectId = require('mongodb').ObjectId; 
 var url = "mongodb://localhost:27017";
-var cors = require("cors");
+
 app.use(cors());
-app.listen(
-    port,
-    () => console.log(`\uD83C\uDF0F running at http://localhost:${port}`)
-);
+
+
+// listing all the  stores 
 app.get('/list', (req, res, next) => {
+
     MongoClient.connect(url, function (err, client) {
+    
         if (err) throw err;
+    
         console.log("connected !");
+    
         var db = client.db("test");
+    
         db.collection('shops').find({}).toArray(function (err, result) {
+    
             if (err) throw err;
-           
+
             res.type('json');
+
             res.end(JSON.stringify(result));
         });
 
     });
 
 });
+
+// listing the specified store using its Id
 app.get('/list/:id', (req, res, next) => {
 
     console.log(req.params.id);
 
     MongoClient.connect(url, function (err, client) {
         if (err) throw err;
+      
         console.log("connected !");
+      
         var db = client.db("test");
-        db.collection('shops').find({_id: ObjectId(req.params.id)}).toArray(function(err, result) {
+      
+        db.collection('shops').find({ _id: ObjectId(req.params.id) }).toArray(function (err, result) {
+      
             if (err) throw err;
-           
+
             res.type('json');
             res.end(JSON.stringify(result));
         });
@@ -43,3 +57,9 @@ app.get('/list/:id', (req, res, next) => {
     });
 
 });
+
+// executing the server and listening for users requests
+app.listen(
+    port,
+    () => console.log(`running at http://localhost:${port}`)
+);
